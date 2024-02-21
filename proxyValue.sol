14f9363c3931aac6de0c4741ba1d11af37b6cd03
ClayYu <32875832+yuhuajing@users.noreply.github.com>
2024-02-21 14:38:45 +0800

    Update proxyValue.sol

--- proxyValue.sol
+++ proxyValue.sol
@@ -1,1 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+
+contract Wallet {
+    address public _beneficiary;
+    error notReceiveData();
+    error WithdrawFailed();
+
+    constructor() payable {
+        _beneficiary = msg.sender;
+    }
+
+    function _mySubfuc() private {
+        if (msg.data.length != 0) {
+            revert notReceiveData();
+        }
+        Address.sendValue(payable(_beneficiary), msg.value);
+    }
+
+    /**
+     * @dev Withdraws funds by owner.
+     */
+    function withdraw() external {
+        require(msg.sender == _beneficiary);
+        uint256 value = address(this).balance;
+        (bool success, ) = msg.sender.call{value: value}("");
+        if (!success) revert WithdrawFailed();
+    }
+
+    receive() external payable {
+        _mySubfuc();
+    }
+}
 
